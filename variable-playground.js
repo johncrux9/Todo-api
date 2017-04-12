6d150c3282fd0275427fdd4d782a9b5aa3756277
David Terry <dgterry@us.ibm.com>
2017-04-12 13:40:04 -0400

    add variable file

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+//     name: 'Dave',
+//     age: 38
+// };
+//
+// function updatePerson(obj) {
+//     // obj = {
+//     //   name: 'Dave',
+//     //     age: 37
+//     // };
+//     obj.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+
+var myArray = [15,37];
+
+function addGrade(myArray) {
+    myArray.push(42);
+    debugger;
+}
+
+addGrade(myArray);
+console.log(myArray);
