158483941ad1c40cbec715a1e04c69e80c8466f8
David Terry <dgterry@us.ibm.com>
2017-04-13 17:20:06 -0400

    db stuff

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,66 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+   dialect: "sqlite",
+    storage: __dirname + "basic-sqlite-database.sqlite"
+});
+
+// model to later sync (so db is created?)
+var Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 250]
+        }
+    },
+    completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+sequelize.sync({
+//    force: true
+}).then(function () {
+    console.log('Everything is synced');
+
+    // fetch todo by its id and print using toJSON else error not found
+    Todo.findById(2).then(function (todo) {
+       if (todo) {
+           console.log(todo.toJSON());
+       } else {
+           console.log('No id of this found');
+       }
+    });
+
+
+
+    // Todo.create({
+    //     description: 'Take out trash'
+    // }).then(function (todo) {
+    //     return Todo.create({
+    //         description: 'Clean office'
+    //     });
+    // }).then(function () {
+    //     // two items created, fetch something
+    //     //return Todo.findById(1);
+    //     return Todo.findAll({
+    //        where: {
+    //            description: {
+    //                $like: '%Office%'
+    //            }
+    //        }
+    //     });
+    // }).then (function (todos) {
+    //     if (todos) {
+    //         todos.forEach(function (todo) {
+    //             console.log(todo.toJSON());
+    //         });
+    //     } else {
+    //         console.log('no todo found!');
+    //     }
+    // }).catch(function (e) {
+    //     console.log(e);
+    // });
+});
