c74188913404954684b9f10cf2c2b6b506b5b97a
David Terry <dgterry@us.ibm.com>
2017-04-14 16:56:10 -0400

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -59,20 +60,30 @@ app.get('/todos/:id', function(req, res){
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    // call create on db.todo
+    //   in then, respond with 200 and value of 200
+    //   catch e, res.status(400).json(e)
+    db.todo.create(body).then(function (todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
 
-    // set body.description to be trimmed value
-    body.description = body.description.trim();
 
-    // add id field to body
-    body["id"] = todoNextId++;
-
-    // add body to todos array, push body into array
-    todos.push(body);
-
-    res.json(body);
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
+    //
+    // // set body.description to be trimmed value
+    // body.description = body.description.trim();
+    //
+    // // add id field to body
+    // body["id"] = todoNextId++;
+    //
+    // // add body to todos array, push body into array
+    // todos.push(body);
+    //
+    // res.json(body);
 });
 
 // DELETE /todos/:id
@@ -120,6 +131,8 @@ app.put('/todos/:id', function(req, res) {
     res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-   console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function() {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
