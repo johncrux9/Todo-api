f6ba4d9a62f524e144554210c3fb0978f0f4d8b1
David Terry <dgterry@us.ibm.com>
2017-04-16 14:26:39 -0400

    security

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ app.post('/users', function (req, res) {
     //   in then, respond with 200 and value of 200
     //   catch e, res.status(400).json(e)
     db.user.create(body).then(function (user) {
-        res.json(user.toJSON());
+        res.json(user.toPublicJSON());
     }, function (e) {
         res.status(400).json(e);
     });
